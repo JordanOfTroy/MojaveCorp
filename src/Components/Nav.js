7fdf1e698e9875a4495d0f0abd72143b4a639639
Jordan Smithson <jordantroysmithson@gmail.com>
2018-12-15 11:34:57 -0700

    started with conditional classnames for burger menu and drawer

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,24 +6,31 @@ class Nav extends Component {
     super(props)
 
     this.state={
-
+      showDrawer: false
     }
+  }
+
+  handleClick = () => {
+    console.log('changing showDrawer in State')
+    this.setState({
+      showDrawer: !this.state.showDrawer
+    })
   }
 
   render() {
     let navImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTADUhTbqbOukvOLy4xDyiecXhs_7q0PX5KBgcJR9OYBhZltM_9Xg'
-    
+    let {showDrawer} = this.state
     return (
       <div id='nav'>
         <img src={navImage} alt="Mojave Corporation Logo" className='nav_logo'/>
         <h1>Mojave Corporation</h1>
-        <ul>
+        <ul className = {showDrawer ? 'open' : 'close'}>
           <li><Link to='/Home' >Home</Link></li>
           <li><Link to='/About' >About</Link></li>
           <li><Link to='/Contact' >Contact</Link></li>
           <li><Link to='/' >Landing</Link></li>
         </ul>
-        <div className="burger_menu">
+        <div className={"burger_menu" + (showDrawer ? ' crossed' : ' bars')} onClick={()=>this.handleClick()}>
           <div className="patty top_patty"></div>
           <div className="patty middle_patty"></div>
           <div className="patty bottom_patty"></div>
