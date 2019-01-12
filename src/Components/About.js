cbeb355f8abdff8a630879f7c58119292f03afc1
Jordan Smithson <jordantroysmithson@gmail.com>
2019-01-12 07:44:42 -0700

    subcids on home still need work for tablet styling. about looks good. moving oneto contact.

--- src/Components/About.js
+++ src/Components/About.js
@@ -25,13 +25,13 @@ class About extends Component {
         <article>
           <p>
             The founders of Mojave Corporation started out in their family cleaning business.  After many years of working in the cleaning and restoration side of the industry, we moved into retail sales and support for the cleaning and restoration industry.  This family also owns and runs several restoration franchises in California dealing with floods, fire and mold damage situations.  </p>
-          <br/><br/>
+          
           <p>
             Being that we only sell products that are tried and tested by our staff in our other businesses, our customers have come to trust our opinions and recommendations.  </p>
-          <br/><br/>
+          
           <p>
             Our heat systems have become a favorite amongst not only restoration contractors but also for pest control companies, hotels and multi-unit housings for the eradication of bed bugs through the use of heat.  Killer Heat was opened to specialize in the equipment, training and consultation services for the hospitality industry in dealing with bed bug infestations.  </p>  
-          <br/><br/>
+          
           <p>
             All the employees of Mojave Corporation are Crime-Free Certified.  We have joined with other great companies who also provide this peace of mind to their customers.  ChooseCrimeFree.com is a group of companies who market together in their own market places letting their community have  place to go to find great companies who care enough to go through such steps.  It's great to know who you are working with and that all the employees of these companies are not only certified crime free upon hiring but each and every year there after.  Besides the extensive background testing that they undergo, they also drug test their employees.  We are all dedicated to providing you excellent customer service with employees who are fantastic. </p>​
         </article>
